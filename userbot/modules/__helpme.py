768fbceeb161b893e69d8eea3d2a4118ebff9a20
DominantUser <69337547+DominantUser@users.noreply.github.com>
2020-08-07 15:24:10 +0300

    Dominant-UserBot

---
+++ userbot/modules/__helpme.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2020 Benita
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+
+# Dominant UserBot 
+
+import asyncio
+import json
+import logging
+import requests
+import userbot
+
+from userbot import CMD_HELP, BOT_USERNAME
+from userbot.events import register
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+@register(outgoing=True, pattern="^.yardım")
+async def yardim(event):
+    tgbotusername = BOT_USERNAME
+    if tgbotusername is not None:
+        results = await event.client.inline_query(
+            tgbotusername,
+            "@DominantUserBot"
+        )
+        await results[0].click(
+            event.chat_id,
+            reply_to=event.reply_to_msg_id,
+            hide_via=True
+        )
+        await event.delete()
+    else:
+        await event.edit("`Bot çalışmıyor! Lütfen Bot Tokeni ve Kullanıcı adını doğru ayarlayın. Modül durduruldu.`")
